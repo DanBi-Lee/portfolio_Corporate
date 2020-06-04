9e42e806d36ed6767a389bfc78ea50b00faf0b48
danbi-Lee <danbi.s.rain@gmail.com>
2020-06-04 10:42:00 +0900

    [#8] gnb수정 - 모바일버전 gnb버튼 수정 - 모바일 gnb 버튼 js 추가 (클릭 시, 메뉴 노출) - 전체 페이지 gnb 적용 - 스크롤 시, header 노출 기능 추가
    resolves #8

--- js/headBox.js
+++ js/headBox.js
@@ -1,28 +1,88 @@
 $(document).ready(function(){
 
-    console.log('test');
-    var $headBox = $('#headBox');
-    var $gnb = $('.gnb')
-    var $gnb_menu = $gnb.children('li');
-    var $gnb_2depth = $gnb.find('.depth2');
-    var $gnb_2depth_menu = $gnb_2depth.children('li');
-    var speed = 400;
+    (function(){
+        var $headBox = $('#headBox');
+        var $gnb = $('.gnb')
+        var $gnb_2depth = $gnb.find('.depth2');
+        var speed = 400;
+        var $btnGnb = $('.btn_gnb');
+        var $mobileGnbMenu = $('.mobile_gnb_menu');
+        var openedMobileGnb = false;
 
-    $gnb.on('mouseenter', openMenu);
-    $gnb.on('focusin', openMenu);
-    $headBox.on('mouseleave', closeMenu);
-    $gnb.last().children('li').last().find('a').on('focusout', closeMenu);
+        function gnbSlide(){
+            $gnb.on('mouseenter', openMenu);
+            $gnb.on('focusin', openMenu);
+            $headBox.on('mouseleave', closeMenu);
+            $gnb.last().children('li').last().find('a').on('focusout', closeMenu);
+        
+            function openMenu(){
+                $headBox.addClass('on');
+                $gnb_2depth.stop().slideDown(speed);
+            }
+            function closeMenu(){
+                $gnb_2depth.stop().slideUp(speed/2,function(){
+                    $headBox.removeClass('on');
+                });
+            }
+        }
+        gnbSlide();
 
-    function openMenu(){
-        $headBox.addClass('on');
-        $gnb_2depth.stop().slideDown(speed);
-    }
+        function headerScroll(){
+            var currentScrollTop = 0;
+            $(window).on('scroll', function(){
+                if( !openedMobileGnb ){
+                    var scrollTop = $(this).scrollTop();
+                    if( currentScrollTop < scrollTop ){
+                        $headBox.css({
+                            marginTop : '-80px'
+                        })
+                    }else{
+                        $headBox.css({
+                            marginTop : '0px'
+                        })
+                    }
+                }
+                currentScrollTop = $(this).scrollTop();
+            })
+        }
+        headerScroll();
 
-    function closeMenu(){
-        $gnb_2depth.stop().slideUp(speed/2,function(){
-            $headBox.removeClass('on');
-        });
-    }
+        function resizing(){ // 리사이징하여 모바일버전, PC버전이 전환될 경우 동작 초기화
+            $(window).on('resize', function(){
+                var _this = $(this);
+                var mbToPcSize = 979;
+                if(mbToPcSize < _this.outerWidth()){//PC화
+                    resetHeader();
+                }
+                function resetHeader(){
+                    $btnGnb.removeClass('on');
+                    $mobileGnbMenu.css({
+                       top : '-100%'
+                    });
+                    openedMobileGnb = false;
+                }
+            });
+        }
+        resizing();
 
-
+        function mobileGnbBtn(){
+            $btnGnb.on('click', function(){
+                var _this = $(this);
+                if(_this.hasClass('on')){
+                    _this.removeClass('on');
+                    $mobileGnbMenu.css({
+                       top : '-100%'
+                    });
+                    openedMobileGnb = false;
+                }else{
+                    openedMobileGnb = true;
+                    _this.addClass('on');
+                    $mobileGnbMenu.css({
+                        top : '0%'
+                    });
+                }
+            });
+        }
+        mobileGnbBtn();
+    })();
 });
